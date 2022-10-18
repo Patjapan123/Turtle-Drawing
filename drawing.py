54ce19b3b7eb614742d058e82113543f3cc3fbee
Patjapan123 <114275013+Patjapan123@users.noreply.github.com>
2022-10-17 22:49:53 -0700

    New Drawing

--- drawing.py
+++ drawing.py
@@ -7,9 +7,16 @@ print("What color do you like your turtle to be?")
 n=input()
 pat = turtle.Turtle()
 pat.color(n)
+print("What color do you like to fill your shape?")
+miles=input()
+pat.fillcolor(miles)
 print("How many sides are your shape?")
 print("This program can generate a shape that has 3 sides to 8 sides")
 p=int(input())
+print("How thick do you want your line to be?")
+number=int(input())
+pat.width(number)
+pat.begin_fill()
 for i in range(p):
     if p == 3:
         pat.forward(100)
@@ -29,7 +36,7 @@ for i in range(p):
     if p == 8:
         pat.forward(100)
         pat.right(45)
-
+pat.end_fill()
 turtle.done()
 
 
