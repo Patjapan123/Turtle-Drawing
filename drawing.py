85a082a82ffce9a6dd570e4c140b5bedf92c0062
Patjapan123 <114275013+Patjapan123@users.noreply.github.com>
2022-10-17 23:10:54 -0700

    AngleFormulaShape

--- drawing.py
+++ drawing.py
@@ -17,25 +17,11 @@ print("How thick do you want your line to be?")
 number=int(input())
 pat.width(number)
 pat.begin_fill()
+angle=eval('180-(180*(p-2)/p)')
 for i in range(p):
-    if p == 3:
+    if p > 2:
         pat.forward(100)
-        pat.right(120)
-    if p == 4:
-        pat.forward(100)
-        pat.right(90)
-    if p == 5:
-        pat.forward(100)
-        pat.right(72)
-    if p == 6:
-        pat.forward(100)
-        pat.right(60)
-    if p == 7:
-        pat.forward(100)
-        pat.right(51.43)
-    if p == 8:
-        pat.forward(100)
-        pat.right(45)
+        pat.right(angle)
 pat.end_fill()
 turtle.done()
 
